5b993a10bd9bf480a82ee38fa9c8f49e2b4ea979
TanyaSoni2912 <tanu139sona@gmail.com>
2025-07-23 11:31:46 +0530

    file structure

---
+++ src/pages/dashboard/dashboard.jsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function dashboard() {
+	return <div>dashboard</div>;
+}
---
+++ src/ui/Sidebar/Sidebar.jsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function Sidebar() {
+	return <div>Sidebar</div>;
+}
