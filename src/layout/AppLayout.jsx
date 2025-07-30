bfdade805e0f381b8765cf0d577e7c36c008a8b3
TanyaSoni2912 <tanu139sona@gmail.com>
2025-07-30 10:33:40 +0530

    SuperAdmin Sidebar rendering on login User Basis

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,9 +1,13 @@
 /** @format */
+import { useSelector } from 'react-redux';
 import Header from '../ui/Header/Header';
 import Sidebar from '../ui/Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
+import SuperAdminSidebar from '../ui/Sidebar/superAdminSidebar/SuperAdminSidebar';
 
 export default function AppLayout() {
+	const { loginUser } = useSelector((state) => state.auth);
+
 	return (
 		<div className='min-h-screen flex flex-col font-inter overflow-x-hidden'>
 			{/* Header */}
@@ -12,7 +16,7 @@ export default function AppLayout() {
 			{/* Main Layout */}
 			<div className='flex flex-1'>
 				{/* Sidebar */}
-				<Sidebar />
+				{loginUser === 1 ? <SuperAdminSidebar /> : <Sidebar />}
 
 				{/* Main Content */}
 				<main className='flex-1 bg-white p-4 overflow-y-auto overflow-x-hidden max-w-full scrollbar-thin scrollbar-thumb-primary-base'>
