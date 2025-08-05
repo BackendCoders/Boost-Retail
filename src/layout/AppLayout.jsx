f5caa2d3f03d0b753659621c2b832db6364716a2
TanyaSoni2912 <tanu139sona@gmail.com>
2025-08-05 16:03:22 +0530

    App Layout fixed now

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -9,18 +9,23 @@ export default function AppLayout() {
 	// const { loginUser } = useSelector((state) => state.auth);
 
 	return (
-		<div className='min-h-screen flex flex-col font-inter overflow-hidden'>
+		<div className='h-screen flex flex-col font-inter overflow-hidden'>
 			{/* Header */}
-			<Header />
+
+			<div className='fixed top-0 left-0 right-0 z-10'>
+				<Header />
+			</div>
 
 			{/* Main Layout */}
-			<div className='flex flex-1'>
+			<div className='flex flex-1 pt-[3.75rem] h-full'>
 				{/* Sidebar */}
 				{/* {loginUser === 1 ? <SuperAdminSidebar /> : <Sidebar />} */}
+
 				<Sidebar />
+
 				{/* Main Content */}
 				<main className='flex-1 bg-light p-4 overflow-y-auto overflow-x-hidden max-w-full scrollbar-thin scrollbar-thumb-primary'>
-					<Outlet />
+					<Outlet /> {/* This will now be the only scrollable area */}
 				</main>
 			</div>
 		</div>
